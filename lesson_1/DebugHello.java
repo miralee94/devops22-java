01ebb18a50c7b7627c832c2b7172e37d8b50af14
Mira Lee <leemira1994@gmail.com>
2023-01-05 14:59:57 +0100

    lesson_1

---
+++ lesson_1/DebugHello.java
@@ -0,0 +1,7 @@
+public class DebugHello {
+    public static void main(String [] args) {
+        System.out.println("Hello World!");
+        System.out.println("Hello World!");
+        System.err.println("Hello World!");
+    }
+}
