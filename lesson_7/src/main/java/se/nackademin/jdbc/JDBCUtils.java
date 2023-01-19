32cbdeed1a1e66a43d3b15e51b27f13f2157bb13
Mira Lee <leemira1994@gmail.com>
2023-01-19 20:53:21 +0100

    added lesson_7

--- lesson_7/src/main/java/se/nackademin/jdbc/JDBCUtils.java
+++ lesson_7/src/main/java/se/nackademin/jdbc/JDBCUtils.java
@@ -5,15 +5,17 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 import java.sql.Statement;
 import java.util.Properties;
+import java.sql.ResultSet;
 
 class JDBCUtils {
-    String hostname = "localhost";
-    String userName = "username";
-    String password = "password"; // Never add hardcoded passwords to your code
-    String port = "3306";
+    String hostname;
+    String userName;
+    String password; // Never add hardcoded passwords to your code
+    String port;
 
-    JDBCUtils(String hostname) {
+    JDBCUtils(String hostname, String port) {
         this.hostname = hostname;
+        this.port = port;
     }
 
     public void setUsername(String userName) {
@@ -25,15 +27,75 @@ class JDBCUtils {
     }
 
     public void createDatabase(Connection conn, String name) throws SQLException {
-        String createString = "create database IF NOT EXISTS " + name;
+        String createString = "create database IF NOT EXISTS mydb";
 
         try (Statement stmt = conn.createStatement()) {
             stmt.executeUpdate(createString);
         } catch (SQLException e) {
             System.out.println(e);
         }
-
     }
+
+    public void createTable(Connection conn) {
+        String sql = """
+            CREATE TABLE IF NOT EXISTS drinks (
+            id MEDIUMINT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+            title TEXT,
+            price DECIMAL(5,2)
+            ) 
+            """;
+            try (Statement stmt = conn.createStatement()) {
+                stmt.executeUpdate(sql);
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+        }
+        public void insertIntoTable(Connection conn) throws SQLException {
+            String insertCappuccino = "INSERT INTO drinks(title, price)" +
+                                        "VALUES('Cappuccino', 55)";
+            String insertCoffee = "INSERT INTO drinks(title, price)" +
+                                    "VALUES('Coffee', 35)";
+
+            try (Statement stmt = conn.createStatement()) {
+                stmt.executeUpdate(insertCappuccino);
+                stmt.executeUpdate(insertCoffee);
+            } catch (SQLException e) {
+                System.out.println(e);
+            }
+        }
+
+        public void readTable(Connection conn, String tableName) throws SQLException {
+            String select = "SELECT * FROM " + tableName;
+
+            try (Statement stmt = conn.createStatement()) {
+                ResultSet rs = stmt.executeQuery(select);
+                while (rs.next()) {
+                    String title = rs.getString(2);
+                    String price = rs.getString(3);
+                    System.out.println(title + " " + price);
+                }
+            } catch (SQLException e) {
+                System.out.println(e);
+            }
+        }
+
+        public void updateValue(Connection conn) throws SQLException {
+            String update = "UPDATE drinks SET price = 65 WHERE title = 'Cappuccino'";
+            try (Statement stmt = conn.createStatement()) {
+                stmt.executeUpdate(update);
+            } catch (SQLException e) {
+                System.out.println(e);
+            }
+        }
+    
+        public void deleteRow(Connection conn) throws SQLException {
+            String update = "DELETE FROM drinks WHERE price = 55";
+            try (Statement stmt = conn.createStatement()) {
+                stmt.executeUpdate(update);
+            } catch (SQLException e) {
+                System.out.println(e);
+            }
+        }
 
     public Connection getConnection() throws SQLException {
 
@@ -43,7 +105,7 @@ class JDBCUtils {
         connectionProps.put("password", this.password);
 
         // Modify the "/" after this.port to set a specific database
-        conn = DriverManager.getConnection("jdbc:mysql://" + this.hostname + ":" + this.port + "/", connectionProps);
+        conn = DriverManager.getConnection("jdbc:mysql://" + this.hostname + ":" + this.port + "/mydb", connectionProps);
 
         System.out.println("Connected to db");
         return conn;
--- lesson_7/src/main/java/se/nackademin/jdbc/Main.java
+++ lesson_7/src/main/java/se/nackademin/jdbc/Main.java
@@ -6,11 +6,17 @@ import java.sql.Connection;
 public class Main {
 
     public static void main(String[] args) throws SQLException {
-        JDBCUtils jdbcUtils = new JDBCUtils("localhost");
+        JDBCUtils jdbcUtils = new JDBCUtils("127.0.0.1", "3306");
         jdbcUtils.setUsername("root"); // Never use the root user in real apps
-        jdbcUtils.setPassword("my-secret-pw"); // Never add hardcoded passwords to your code
+        jdbcUtils.setPassword("miradocker94"); // Never add hardcoded passwords to your code
 
         Connection conn = jdbcUtils.getConnection();
-        jdbcUtils.createDatabase(conn, "example"); // This will create the database with no tables
+        jdbcUtils.createDatabase(conn, "mydb"); // This will create the database with no tables
+        jdbcUtils.createTable(conn);
+        jdbcUtils.insertIntoTable(conn);
+        jdbcUtils.readTable(conn, "drinks");
+        jdbcUtils.updateValue(conn);
+        jdbcUtils.deleteRow(conn);
+        jdbcUtils.readTable(conn, "drinks");
     }
 }
